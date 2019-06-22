d39e9281cd3593e36dca99dab18f02cb18fe6209
constconsole <constconsole@gmail.com>
2019-06-22 21:29:31 +0530

    8_connect_redis

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import * as Store from 'connect-redis';
 import * as dotenv from 'dotenv';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
 import { SESSION_SECRET } from './constants';
+import { redis } from './redis';
 
 dotenv.config();
 
 async function bootstrap() {
+  const RedisStore = Store(session);
+
   const app = await NestFactory.create(AppModule);
   app.use(
     session({
+      store: new RedisStore({
+        client: redis as any,
+      }),
       name: 'votinapp',
       secret: SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
-      cookie: { httpOnly: true, secure: process.env.NODE_ENV === 'production' },
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        expires: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+      },
     }),
   );
   await app.listen(3000);
