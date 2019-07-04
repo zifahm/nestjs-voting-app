b95dfa1506f35220ec39dbc25181a9a0ae669257
zifahm <muhamm3dhafiz@gmail.com>
2019-07-04 16:47:28 +0530

    26_header_title

--- packages/app/src/navigation/HomeTabNavigator.tsx
+++ packages/app/src/navigation/HomeTabNavigator.tsx
@@ -1,22 +1,54 @@
 import {
   createBottomTabNavigator,
-  NavigationContainer
+  NavigationContainer,
+  NavigationScreenConfig,
+  NavigationStackScreenOptions
 } from "react-navigation";
 import PollScreen from "../screens/PollScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import { ROUTES, Stack } from "../types/stack";
 
+const NavigationOptions: NavigationScreenConfig<
+  NavigationStackScreenOptions
+> = ({ navigation }) => {
+  let headerTitle: string;
+
+  const { routeName } = navigation.state.routes[navigation.state.index];
+
+  switch (routeName) {
+    case ROUTES.HomePoll:
+      headerTitle = "Poll";
+      break;
+    case ROUTES.HomeProfile:
+      headerTitle = "Profile";
+      break;
+  }
+  return {
+    headerTitle
+  };
+};
+
 const HomeTabNavigator = (stack: Stack): NavigationContainer => {
   switch (stack) {
     case ROUTES.RootUnAuth:
-      return createBottomTabNavigator({
-        [ROUTES.HomePoll]: PollScreen
-      });
+      return createBottomTabNavigator(
+        {
+          [ROUTES.HomePoll]: PollScreen
+        },
+        {
+          navigationOptions: NavigationOptions
+        }
+      );
     case ROUTES.RootApp:
-      return createBottomTabNavigator({
-        [ROUTES.HomePoll]: PollScreen,
-        [ROUTES.HomeProfile]: ProfileScreen
-      });
+      return createBottomTabNavigator(
+        {
+          [ROUTES.HomePoll]: PollScreen,
+          [ROUTES.HomeProfile]: ProfileScreen
+        },
+        {
+          navigationOptions: NavigationOptions
+        }
+      );
   }
 };
 
--- packages/app/src/navigation/MainStackNavigator.tsx
+++ packages/app/src/navigation/MainStackNavigator.tsx
@@ -1,10 +1,26 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
 import { createStackNavigator, NavigationContainer } from "react-navigation";
 import { ROUTES, Stack } from "../types/stack";
 import HomeTabNavigator from "./HomeTabNavigator";
 
 const MainStackNavigator = (stack: Stack): NavigationContainer => {
-  return createStackNavigator({
-    [ROUTES.MainHome]: HomeTabNavigator(stack)
-  });
+  return createStackNavigator(
+    {
+      [ROUTES.MainHome]: HomeTabNavigator(stack)
+    },
+    {
+      defaultNavigationOptions: ({ navigation }) => ({
+        headerLeft: (
+          <Ionicons
+            name="md-menu"
+            size={30}
+            style={{ padding: 10 }}
+            onPress={() => navigation.openDrawer()}
+          />
+        )
+      })
+    }
+  );
 };
 export default MainStackNavigator;
